16af8f02a29dba33f98413b5ca8d228aa588a338
iamrdev33 <iamrdev33@gmail.com>
2025-02-22 02:03:27 +0200

    Enhance navbar with dropdown menus for Tattoo Guide and Help & Care sections;

---
+++ app/(site)/aftercare/page.tsx
@@ -0,0 +1,11 @@
+import { FaOtter } from "react-icons/fa6";
+
+export default function AfterCare() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">After Care Page Under Construction</h1>
+      <p className="text-lg mb-8">This page is currently under construction. Please check back later.</p>
+      <FaOtter className="w-16 h-16" />
+    </div>
+  );
+}
--- app/(site)/components/navbar.tsx
+++ app/(site)/components/navbar.tsx
@@ -3,19 +3,34 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
-import { FaInstagram, FaFacebookF } from "react-icons/fa6";
+import { FaCaretUp, FaCaretDown, FaFacebookF, FaInstagram } from "react-icons/fa6";
 
 const navLinks = [
   { href: "/", label: "Home" },
   { href: "/portofolio", label: "Portofolio" },
   { href: "/book-appointment", label: "Book Appointment" },
-  { href: "/faq", label: "FAQ" },
-  { href: "/flash", label: "Tattoo Flash" },
+  {
+    title: 'Tattoo Guide',
+    links: [
+      { href: "/pain-chart", label: "Pain Chart" },
+      { href: "/quiz", label: "Quiz" },
+    ]
+  },
+  {
+    title: 'Help & Care',
+    links: [
+      { href: "/faq", label: "FAQ" },
+      { href: "/aftercare", label: "Aftercare" },
+    ]
+  }
+
 ];
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
   const [visible, setVisible] = useState(true);
+  const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
+
   let lastScrollY = 0;
 
   useEffect(() => {
@@ -44,17 +59,47 @@ export default function Navbar() {
         <span> Inks</span>
       </h1>
       <ul className="flex justify-center items-center gap-3">
-        {navLinks.map(({ href, label }) => (
-          <li key={href}>
-            <Link
-              href={href}
-              className={`px-4 py-2 rounded-md transition duration-300 ${pathname === href ? "text-primary" : "hover:text-primary"
-                }`}
+        {navLinks.map((link, index) =>
+          link.links ? (
+            <li
+              key={link.title}
+              className="relative"
+              onMouseEnter={() => setActiveDropdown(link.title)}
+              onMouseLeave={() => setActiveDropdown('')}
             >
-              {label}
-            </Link>
-          </li>
-        ))}
+              <div className="hover:underline flex items-center gap-2" >
+                {link.title} {activeDropdown == link.title ? <FaCaretUp /> : <FaCaretDown /> }
+              </div>
+              {activeDropdown == link.title &&
+                <ul className="absolute top-full left-0 bg-grey border shadow-lg w-40 p-2">
+
+                  {link.links.map(({ href, label }) =>
+                    <li key={href}>
+                      <Link
+                        href={href}
+                        className={`px-4 py-2 rounded-md transition duration-300 ${pathname === href ? "text-primary" : "hover:text-primary"
+                          }`}
+                      >
+                        {label}
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              }
+            </li>
+          )
+            :
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={`px-4 py-2 rounded-md transition duration-300 ${pathname === link.href ? "text-primary" : "hover:text-primary"
+                  }`}
+              >
+                {link.label}
+              </Link>
+            </li>
+        )}
+
         <li className="flex flex-row gap-4">
           <Link
             href={'https://www.facebook.com/profile.php?id=61572642881407'}
@@ -65,7 +110,7 @@ export default function Navbar() {
           <Link
             href={'https://instagram.com/kalos_inks'}
             target="_blank"
-            >
+          >
             <FaInstagram />
           </Link>
         </li>
